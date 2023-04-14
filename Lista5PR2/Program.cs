5bfac76733f023b6f480e6ddfb31ebb5f4de11be
muril <muril@DESKTOP-R1PTDFL>
2023-04-14 17:50:59 -0300

    Adicionar arquivos de projeto.

---
+++ Lista5PR2/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista5RP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //exerc 1
+            //Console.Write("Digite um número inteiro: ");
+            //int numero = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= numero; i++)
+            //{
+            //    Console.WriteLine(i);
+
+            //exerc 2
+
+            //Console.Write("Digite um número inteiro: ");
+            //    int nmr = int.Parse(Console.ReadLine());
+
+            //    for (int i = 2; i <= nmr; i += 2)
+            //    {
+            //        Console.WriteLine(i);
+
+            //exerc 3
+            //    int nmr;
+            //        Console.WriteLine("digite um numero maior q 1000: ");
+            //        nmr = int.Parse(Console.ReadLine());
+
+            //        for (int i = 1000; i <= nmr; i += 2)
+            //        {
+            //            Console.WriteLine(i);
+            //        }
+
+            // exerc 4
+            //int soma = 0;
+
+            //while (soma < 200)
+            //{
+            //    Console.Write("Digite um número inteiro: ");
+            //    int num = int.Parse(Console.ReadLine());
+
+            //    if (num > 0)
+            //    {
+            //        Console.WriteLine(num);
+            //        soma += num;
+            //    }
+            //}
+
+            //Console.WriteLine("Soma dos números positivos: " + soma);
+
+            //exerc5
+
+            //Console.Write("Digite um número inteiro: ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Divisores de " + num + ":");
+
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    if (num % i == 0)
+            //    {
+            //        Console.WriteLine(i);
+
+            //exerc6
+
+            //        int maior = int.MinValue;
+            //        int menor = int.MaxValue;
+
+            //        for (int i = 1; i <= 10; i++)
+            //        {
+            //            Console.Write("Digite o " + i + "º número: ");
+            //            int num = int.Parse(Console.ReadLine());
+
+            //            if (num > maior)
+            //            {
+            //                maior = num;
+            //            }
+
+            //            if (num < menor)
+            //            {
+            //                menor = num;
+            //            }
+            //        }
+
+            //        Console.WriteLine("O maior número digitado foi: " + maior);
+            //        Console.WriteLine("O menor número digitado foi: " + menor);
+
+            //exerc7
+
+            //int somaPares = 0;
+            //int somaImpares = 0;
+            //int num;
+
+            //do
+            //{
+            //    Console.Write("Digite um número (ou zero para sair): ");
+            //    num = int.Parse(Console.ReadLine());
+
+            //    if (num % 2 == 0)
+            //    {
+            //        somaPares += num;
+            //    }
+            //    else
+            //    {
+            //        somaImpares += num;
+            //    }
+
+            //} while (num != 0);
+
+            //Console.WriteLine("Soma dos números pares: " + somaPares);
+            //Console.WriteLine("Soma dos números ímpares: " + somaImpares);
+
+
+
+
+
+        }
+    }
+}
+    
+      
